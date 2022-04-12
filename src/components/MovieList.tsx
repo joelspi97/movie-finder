22f49e08ae2d7593b3010db7fb650c8e4f075bd7
Joel Spinelli <joelspi97@gmail.com>
2022-04-12 20:52:03 -0300

    added the scaffolding for the API call logic

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -3,39 +3,53 @@ import { connect } from 'react-redux';
 import Movie from './Movie';
 import axios, { Canceler } from 'axios';
 import '../scss/components/MovieList.scss';
+import OmdbResponseJson from '../interfaces/omdbJson';
 
 function MovieList() {
-  const loadingNextPage = true;
   const [movies, setMovies] = useState<Array<any>>([]);
   const [query, setQuery] = useState<string>('');
+  const [movieNotFound, setMovieNotFound] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const lastMovie = createRef();
 
   useEffect(() => {
-    let MOVIES_URL: string;
+    let cancel: Canceler;
     const API_KEY: string = 'bc9de2ce&';
+    let MOVIES_URL: string = 'http://www.omdbapi.com/?&apikey='.concat(API_KEY);
+    setLoading(true);
+
     if (query.length > 2) {
-      MOVIES_URL = 'http://www.omdbapi.com/?s='.concat(query).concat('&apikey=').concat(API_KEY);
+      MOVIES_URL = MOVIES_URL.concat(`s=${query}`);
+      axios({
+        method: 'GET',
+        url: MOVIES_URL,
+        cancelToken: new axios.CancelToken(c => cancel = c)
+      })
+        .then(res => {
+          console.log(res.data);
+  
+          if(res.data.Error) {
+            setMovieNotFound(true);
+            setMovies([]);
+          } else {
+            setMovieNotFound(false);
+            setMovies(res.data.Search);
+          }
+
+          setLoading(false);
+        })
+        .catch(err => {
+          if (axios.isCancel(err)) return;
+          setMovies([]);
+          console.error(err);
+        });
     } else {
-      MOVIES_URL = 'http://www.omdbapi.com/?s='.concat('avengers').concat('&apikey=').concat(API_KEY);
+      setMovieNotFound(false);
+      setMovies([]);
+      return;
     };
 
-    let cancel: Canceler;
-    axios({
-      method: 'GET',
-      url: MOVIES_URL,
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    })
-      .then(res => {
-        console.log(res.data);
-        setMovies(res.data.Search);
-      })
-      .catch(err => {
-        if (axios.isCancel(err)) return;
-        
-        console.error(err);
-        setMovies([]);
-      });
     return () => cancel();
   }, [query]);
 
@@ -43,8 +57,8 @@ function MovieList() {
     <div className="movie-list">
       <div className="movie-list__text-wrapper">
         <h2>Welcome!</h2>
-        <p>This website will help you to find information about movies you're interested in.</p>
-        <p className="mb-5" id="search-bar-instructions">Use the search bar to search for particular titles.</p>
+        <p>This website features an infinite scroll that gets data from a RESTful API</p>
+        <p className="mb-5" id="search-bar-instructions">Use the search bar to filter for particular titles.</p>
       </div>
       <input 
         aria-labelledby="search-bar-instructions"
@@ -54,37 +68,41 @@ function MovieList() {
         value={query}
         onChange={e => setQuery(e.target.value)}
       />
-      <ul className="movie-list__list">
-        {
-          movies.map((movie: any, index: number): JSX.Element => {
-            if (movies.length === index + 1) {
-              return (
-                <Movie 
-                  ref={lastMovie}
-                  key={movie.imdbID} 
-                  title={movie.Title}
-                  posterUrl={movie.Poster}
-                  lastMovie={true}
-                />
-              )  
-            }
-            return (
-              <Movie 
-                key={movie.imdbID} 
-                title={movie.Title}
-                posterUrl={movie.Poster}
-              />
+      
+      {
+        (movies.length === 0 && query.length < 3)
+          ? <p className="text-white h2">The results of your search will appear here!</p> 
+          : (loading 
+              ? <div className="movie-list__loading">...</div> // Acá agregar un loading spinner
+              : (movieNotFound 
+                  ? <p className="text-white h2">We haven't found movie titles that contain that...</p>
+                  : <ul className="movie-list__list">
+                    {
+                      movies.map((movie: any, index: number): JSX.Element => {
+                        if (movies.length === index + 1) {
+                          return (
+                            <Movie 
+                              ref={lastMovie}
+                              key={movie.imdbID} 
+                              title={movie.Title}
+                              posterUrl={movie.Poster}
+                              lastMovie={true}
+                            />
+                          )  
+                        }
+                        return (
+                          <Movie 
+                            key={movie.imdbID} 
+                            title={movie.Title}
+                            posterUrl={movie.Poster}
+                          />
+                        )
+                      })
+                    }
+                    </ul> 
+                )
             )
-          })
-        }
-
-        {
-          // Acá agregar un loading spinner
-          loadingNextPage && (
-            <div className="movie-list__loading">...</div>
-          )
-        }
-      </ul>
+      }
     </div>
   );
 };
