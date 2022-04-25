30d362bbef3d8d877473b55506d1c54b04c8b96a
Joel Spinelli <joelspi97@gmail.com>
2022-04-24 22:30:49 -0300

    finished the infinite scroll logic and made a custom hook to contain it

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -3,89 +3,19 @@ import { connect } from 'react-redux';
 import Movie from './Movie';
 import axios from 'axios';
 import '../scss/components/MovieList.scss';
-import TmdbResponseJson from '../interfaces/tbdbJson';
+//import TmdbResponse from '../interfaces/tmdbJson';
+import useMovieSearch from '../hooks/useMovieSearch';
 
 function MovieList() {
-  const [movies, setMovies] = useState<Array<any>>([]);
-  const [pageNumber, setPageNumber] = useState<number>(1);
   const [query, setQuery] = useState<string>('');
-  const [movieNotFound, setMovieNotFound] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [apiError, setApiError] = useState<boolean>(false);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-
+  const [pageNumber, setPageNumber] = useState<number>(1);
   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/';
-  
-  //
-  //
-  // Hacer un hook con todo esto
-  
-  const API_KEY = '291a10a46f07867159009943d2d6daa0';
-  const BASE_URL = 'https://api.themoviedb.org/3/';
-  let currentUrl: string;
 
-  //Este bloque aca afuera esta raro
-  if (query.length > 0) {
-    currentUrl = BASE_URL.concat(`search/movie?query=${query}`).concat(`&api_key=${API_KEY}`);
-  } else {
-    currentUrl = BASE_URL.concat(`movie/popular?page=${pageNumber}`).concat(`&api_key=${API_KEY}`);
-  }
-  //
-  
-  useEffect(() => {
-    setLoading(true);
-    setApiError(false);
-    setPageNumber(1);
-    setHasMore(true);
-    let controller = new AbortController;
-    
-    axios.get<TmdbResponseJson>(currentUrl, {
-      signal: controller.signal
-    })
-    .then(res => {
-      console.log(res.data);
-
-      if (res.data.page === res.data.total_pages) setHasMore(false);
-      
-      if(res.data.results.length === 0) {
-        setMovieNotFound(true);
-        setMovies([]);
-      } else {
-        setMovieNotFound(false);
-        setMovies(res.data.results);
-      }
-
-      setLoading(false);
-    })
-    .catch(err => {
-      if (axios.isCancel(err)) return;
-      
-      setApiError(true);
-      setMovies([]);
-      console.error(err);
-    });
-    
-    return () => controller.abort();
-  }, [query]);
-  
-  //
-  //
-  //
-
-  function getNextPage(): void {
-    console.log(pageNumber);
-    currentUrl = currentUrl.concat(`&page=${pageNumber}`); // Esto no funca, en parte porque la busqueda por defecto no podria funcionar de por si
-
-    axios.get<TmdbResponseJson>(currentUrl)
-      .then(res => {
-        if (res.data.page === res.data.total_pages) setHasMore(false);
-
-        setMovies(prevMovies => [...prevMovies, res.data.results]);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
+  const { loading,
+          apiError,
+          movies,
+          movieNotFound,
+          hasMore } = useMovieSearch(query, pageNumber);
 
   const observerRef = useRef<IntersectionObserver>();
   const lastMovie = useCallback((node: HTMLLIElement) => {
@@ -94,15 +24,17 @@ function MovieList() {
 
     observerRef.current = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting && hasMore) { 
-        console.log(pageNumber);
-        setPageNumber(prevPageNumber => prevPageNumber + 1); // Esto no funca..?
-        console.log(pageNumber);
-        getNextPage();
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
       };
     });
 
     if (node) observerRef.current.observe(node);
   }, [loading, hasMore]);
+
+  function handleSearch(e: React.ChangeEvent<HTMLInputElement>): void {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
 
   return (
     <div className="movie-list">
@@ -117,7 +49,7 @@ function MovieList() {
         type="text" 
         placeholder="Search movies by title..."
         value={query}
-        onChange={e => setQuery(e.target.value)}
+        onChange={handleSearch}
       />
       
       { apiError && <p className="text-white h2">There was an error with your search, please try again later</p> }
@@ -130,7 +62,7 @@ function MovieList() {
       { movieNotFound && <p className="text-white h2">We haven't found movie titles that contain that. Please try typing something different</p> }
 
       {
-        (movies.length > 0 && !apiError && !loading && !movieNotFound) && (
+        (movies.length > 0 && !apiError && !movieNotFound) && (
           <ul className="movie-list__list">
             {
               movies.map((movie: any, index: number): JSX.Element => {                
--- src/hooks/useMovieSearch.ts
+++ src/hooks/useMovieSearch.ts
@@ -1,7 +1,64 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import TmdbResponse from "../interfaces/tmdbJson";
 
-function useMovieSearch() {
-  return null;
+export default function useMovieSearch(query: string, pageNumber: number) {
+  const [movies, setMovies] = useState<Array<any>>([]);
+  const [movieNotFound, setMovieNotFound] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [apiError, setApiError] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+    
+  const API_KEY = '291a10a46f07867159009943d2d6daa0';
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  let currentUrl: string;
+
+  if (query.length > 0) {
+    currentUrl = BASE_URL.concat(`search/movie?query=${query}&page=${pageNumber}`).concat(`&api_key=${API_KEY}`);
+  } else {
+    currentUrl = BASE_URL.concat(`movie/popular?page=${pageNumber}`).concat(`&api_key=${API_KEY}`);
+  }
+
+  useEffect(() => {
+    setMovies([]);
+  }, [query]);
+
+  useEffect(() => {
+    setLoading(true);
+    setApiError(false);
+    setMovieNotFound(false);
+    let controller = new AbortController;
+    
+    axios.get<TmdbResponse>(currentUrl, {
+      signal: controller.signal
+    })
+    .then(res => {
+      console.log(res.data);
+
+      if(res.data.results.length === 0) {
+        setMovieNotFound(true);
+        setMovies([]);
+      } else {
+        setMovies(prevMovies => Array.from( new Set([...prevMovies, ...res.data.results]) ));
+      }
+      
+      setHasMore(res.data.page !== res.data.total_pages);
+      setLoading(false);
+    })
+    .catch(err => {
+      if (axios.isCancel(err)) return;
+      
+      setApiError(true);
+      setMovies([]);
+      console.error(err);
+    });
+    
+    return () => controller.abort();
+  }, [query, pageNumber]);
+
+  return { loading,
+           apiError,
+           movies,
+           movieNotFound,
+           hasMore };
 }
-
-export default useMovieSearch;
